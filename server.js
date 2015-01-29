1ff59bdb3b4bc1080a129c7fb1d714625e782181
brad <lyman.brad3211@gmail.com>
2015-01-28 19:08:37 -0500

    created a simple node server to serve the website'

---
+++ server.js
@@ -0,0 +1,21 @@
+var
+  express = require( 'express' ),
+  path    = require('path'),
+
+  app     = express(),
+  port    = process.argv[2] || 8080;
+
+app.use( '/', function( req, res, next ) {
+  console.log( 'request on: ' + req.url );
+  next( null, req, res );
+});
+
+app.use( '/js', express.static( path.join( __dirname, 'js' ) ) );
+
+app.get( '/', function( req, res ) {
+  res.sendFile( path.join( __dirname, 'threeBodySim.html' ) );
+});
+
+app.listen( port, function() {
+  console.log( 'listening on port ' + port );
+});
